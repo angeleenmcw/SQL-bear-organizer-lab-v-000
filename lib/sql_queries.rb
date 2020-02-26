c4db80df3a343e42e5da91471e70daeb7b708b1b
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-26 09:40:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,15 +18,13 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age 
-  FROM bears
-  ORDER BY DESC LIMIT 1;"
+  "SELECT name, MAX(age) 
+  FROM bears;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age
-  FROM bears
-  ORDER BY ASC LIMIT 1;"
+  "SELECT name,MIN(age)
+  FROM bears;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
